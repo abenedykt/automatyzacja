52a31fde19262aa947623a7bf87ede06a7ef6b09
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2017-11-28 12:10:57 +0100

    added more tests

--- Automatyzacja2017/Automatyzacja2017/MathematicsTests.cs
+++ Automatyzacja2017/Automatyzacja2017/MathematicsTests.cs
@@ -16,5 +16,44 @@ namespace Automatyzacja2017
             // assert
             Assert.Equal(30, result);
         }
+
+        [Fact]
+        public void Method_substract_returns_sum_of_given_values()
+        {
+            // arrange
+            var math = new Mathematics();
+
+            // act
+            var result = math.Substract(10, 20);
+
+            // assert
+            Assert.Equal(-10, result);
+        }
+
+        [Fact]
+        public void Method_multiply_returns_sum_of_given_values()
+        {
+            // arrange
+            var math = new Mathematics();
+
+            // act
+            var result = math.Multiply(10, 20);
+
+            // assert
+            Assert.Equal(200, result);
+        }
+
+        [Fact]
+        public void Method_divide_returns_sum_of_given_values()
+        {
+            // arrange
+            var math = new Mathematics();
+
+            // act
+            var result = math.Divide(25, 5);
+
+            // assert
+            Assert.Equal(5, result);
+        }
     }
 }
