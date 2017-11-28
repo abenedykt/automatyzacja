c3ad9c78f94a757dc9e68a2783e0c3e851da5505
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2017-11-28 12:00:49 +0100

    added first test for adding

---
+++ Automatyzacja2017/Automatyzacja2017/MathematicsTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Automatyzacja2017
+{
+    public class MathematicsTests
+    {
+        [Fact]
+        public void Method_add_returns_sum_of_given_values()
+        {
+            // arrange
+            var math = new Mathematics();
+
+            // act
+            var result = math.Add(10, 20);
+
+            // assert
+            Assert.Equal(30, result);
+        }
+    }
+}
