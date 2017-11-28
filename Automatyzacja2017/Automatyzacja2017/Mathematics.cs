c8bdc1004ba15c7e3280df854d0cea597bd9fe10
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2017-11-28 11:24:30 +0100

    added mathematics class for future testing

---
+++ Automatyzacja2017/Automatyzacja2017/Mathematics.cs
@@ -0,0 +1,25 @@
+﻿namespace Automatyzacja2017
+{
+    public class Mathematics
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Substract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Divide(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
